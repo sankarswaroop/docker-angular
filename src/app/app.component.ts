65898ab31b99977ee3add1fffca26f516418cf8a
sankarswaroop <sankarasapu@gmail.com>
2018-08-13 21:20:37 -0400

    angular using CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+export class User {
+	fName: string;
+	lName: string;
+	age: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'docker-angular';
+  constructor(private http: HttpClient) {}
+
+	rows: User[] = [];
+
+	ngOnInit(){
+		this.http.get('http://localhost:8090/list').subscribe((response: User[]) => {
+			this.rows = response;
+		});
+	}
 }
